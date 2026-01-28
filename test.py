890e50f6def5c8c614fc6009f2bef99054a5b156
lunraksa <lunraksa148@gmail.com>
2026-01-28 20:53:25 +0700

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+print("Welcome to my Web")
+A=input(int("Your favorite number! "))
+if A>5:
+    print("I love you!")
+elif  A<5:
+    print ("Do you love me! ")
+else:
+    print("You not my type!")
+ 
